b3cb996beaf56b96bf25d3d019f13c8ece0d533c
Francielly Nascimento <116462191+Francielly-Nascimento@users.noreply.github.com>
2022-10-24 14:29:19 -0300

    Add files via upload

---
+++ beecrowd simple sum/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{ int A,B,SOMA;
+  printf("Digite dois numeros inteiros.");
+  scanf("%d %d", &A,&B);
+  SOMA=A+B;
+  printf("SOMA = %d", SOMA);
+    return 0;
+}
